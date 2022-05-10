e4448604574b98986399e3ce470080891eeed367
Thomas Bury <tombury182@gmail.com>
2022-05-10 19:00:51 -0400

    first run

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
-from app_functions import phase_map, generate_phase_trajectory, make_phase_map
+from app_functions import generate_cobweb_trajectory, make_cobweb_fig, make_restitution_fig
+
 
 
 #-------------
@@ -31,13 +32,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # Get mathjax for latex
 external_scripts = ['https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML']
 
-
-
 app = dash.Dash(__name__, 
 				external_stylesheets=external_stylesheets,
                 external_scripts = external_scripts,
 				)
-
 
 print('Launching dash')
 server = app.server
@@ -47,43 +45,68 @@ server = app.server
 # Generate figures
 #-------------------
 
+# Default model parameter values
+y0 = 120
+a = 96.9
+b = 0.0104
+theta = 20
+ts = 400
 
-# Defualt values for simulation
-phi0 = 0.2 # initial condition
-nmax = 2 # number of iterations
+# Defualt simulation parameters
+apd0 = 150 
+nmax = 40
 
-# Create phase map function (might want to add extra parameters here)
-def phase_map_specific(phi):
-    return phase_map(phi)
+# Generate cobweb trajectory
+apd_traj = generate_cobweb_trajectory(nmax, apd0, y0, a, b, theta, ts)
 
-# Generate phi trajectory
-phi_traj = generate_phase_trajectory(phase_map_specific, nmax, phi0)
-
-
-# Make figure of phase map and stable trajectory
-fig_phase_map = make_phase_map(phase_map_specific,
-                               phi_traj,
-                               )
-
+# Make figures
+fig_cobweb = make_cobweb_fig(y0, a, b, theta, ts, apd_traj)
+fig_restitution = make_restitution_fig(y0, a, b)
 
 #--------------------
 # App layout
 #–-------------------
 
-
 # Font sizes
 size_slider_text = '15px'
 size_title = '30px'
 
-
 # Parameter bounds
-phi0_min = 0
-phi0_max = 1
-phi0_marks = {x:str(round(x,2)) for x in np.arange(0,1.1,0.2)}
+apd0_min = 0
+apd0_max = 250
+# apd0_step = 1
+# apd0_marks = {x:str(x) for x in np.arange(apd0_min,apd0_max,100)}
+
 
 nmax_min = 0
-nmax_max = 1000
-nmax_marks = {float(x):str(round(x,2)) for x in np.arange(0,nmax_max,100)}
+nmax_max = 100
+# nmax_step = 20
+# nmax_marks = {float(x):str(round(x,2)) for x in np.arange(nmax_min,nmax_max,20)}
+
+
+y0_min = 0
+y0_max = 200
+# y0_step = 10
+# y0_marks = {float(x):str(round(x,2)) for x in np.arange(y0_min,y0_max,10)}
+
+
+a_min = 0
+a_max = 200
+a_marks = {float(x):str(round(x,2)) for x in np.arange(a_min,a_max,10)}
+
+b_min = 0.005
+b_max = 0.015
+b_marks = {float(x):str(round(x,4)) for x in np.arange(b_min,b_max,0.002)}
+
+theta_min = 0
+theta_max = 50
+# theta_step = 20
+theta_marks = {float(x):str(round(x,2)) for x in np.arange(theta_min,theta_max,10)}
+
+ts_min = 100
+ts_max = 500
+# ts_step = 100
+# ts_marks = {float(x):str(round(x,2)) for x in np.arange(ts_min,ts_max,100)}
 
 
 # # PDF image of text
@@ -92,16 +115,6 @@ nmax_marks = {float(x):str(round(x,2)) for x in np.arange(0,nmax_max,100)}
 
 
 
-model_text = \
-'''
-Phase map:
-$$ x_{t+1} = 
-\\begin{cases}
-\\frac{3}{2} x_t & 0 \leq x_t < \\frac{2}{3},\\\\
-\\frac{3}{2} x_t - 1 & \\frac{2}{3} \leq x_t < 1
-\\end{cases}
-$$
-'''
 
 model_text_2_line1 = \
 '''
@@ -115,7 +128,6 @@ x_{t+1} = (3/2)x_t - 1 for 2/3 <= x_t < 1
 
 app.layout = html.Div([
       
-    
     # html.Div(
     #     html.H1('Cobweb plot',
     #              	style={'textAlign':'center',
@@ -127,90 +139,196 @@ app.layout = html.Div([
     # Left half of app
  	html.Div([
         
-        
-        html.H1('Cobweb plot',
-                 style={'textAlign':'left',
+        html.H1('Restitution analysis',
+                  style={'textAlign':'left',
                         'fontSize':size_title,
                         'color':'black'}
-                ),
-        
+        ),
 
-        
-        # Model description
-        html.P(model_text_2_line1,
-                style={'fontSize':15,
-                      'color':'black',
-                      # 'textAlign':'left',
-                      }),
-        html.Br(),
-        html.P(model_text_2_line2,
-                style={'fontSize':15,
-                      'color':'black',
-                      # 'textAlign':'left',
-                      }),        
-         
-        html.Br(),
-        
-        # Slider for phi0
-		html.Label('Initial condition = {}'.format(phi0),
- 				   id='phi0_slider_text',
+        # Slider for apd0
+		html.Label('APD_0 = {}'.format(apd0),
+ 				   id='apd0_slider_text',
  				   style={'fontSize':size_slider_text}),  
- 				   
-		dcc.Slider(id='phi0_slider',
- 				   min=phi0_min, 
- 				   max=phi0_max, 
- 				   step=0.001,
- 				   marks=phi0_marks,
- 				   value=phi0
-		),          
-         
+		dcc.Slider(id='apd0_slider',
+ 				   min=apd0_min, 
+ 				   max=apd0_max, 
+ 				   # step=apd0_step,
+ 				   # marks=apd0_marks,
+ 				   value=apd0,
+		),
+        
          
 		# Slider for nmax
-		html.Label('Number of iterations = {}'.format(nmax),
+		html.Label('Nmax = {}'.format(nmax),
  				   id='nmax_slider_text',
  				   style={'fontSize':size_slider_text}),  
  				   
 		dcc.Slider(id='nmax_slider',
  				   min=nmax_min, 
  				   max=nmax_max, 
- 				   step=1.0, 
- 				   marks=nmax_marks,
+ 				   # step=nmax_step, 
+ 				   # marks=nmax_marks,
  				   value=nmax
 		),
+
+        
+        # Slider for y0
+		html.Label('Initial condition = {}'.format(y0),
+ 				   id='y0_slider_text',
+ 				   style={'fontSize':size_slider_text}),  
+ 				   
+		dcc.Slider(id='y0_slider',
+ 				   min=y0_min, 
+ 				   max=y0_max, 
+ 				   # step=y0_step,
+ 				   # marks=y0_marks,
+ 				   value=y0
+		),   
+
+        
+        # Slider for a
+		html.Label('Initial condition = {}'.format(a),
+ 				   id='a_slider_text',
+ 				   style={'fontSize':size_slider_text}),  
+ 				   
+		dcc.Slider(id='a_slider',
+ 				   min=a_min, 
+ 				   max=a_max, 
+ 				   # step=a_step,
+ 				   # marks=a_marks,
+ 				   value=a
+		),           
         
         
-        ],       
-		style={'width':'30%',
-			   'height':'500px',
+        # Slider for b
+		html.Label('Initial condition = {}'.format(b),
+ 				   id='b_slider_text',
+ 				   style={'fontSize':size_slider_text}),  
+ 				   
+		dcc.Slider(id='b_slider',
+ 				   min=b_min, 
+ 				   max=b_max, 
+ 				   # step=b_step,
+ 				   marks=b_marks,
+ 				   value=b
+		),           
+        
+        
+        
+        # Slider for theta
+		html.Label('Initial condition = {}'.format(theta),
+ 				   id='theta_slider_text',
+ 				   style={'fontSize':size_slider_text}),  
+ 				   
+		dcc.Slider(id='theta_slider',
+ 				   min=theta_min, 
+ 				   max=theta_max, 
+ 				   # step=theta_step,
+ 				   # marks=theta_marks,
+ 				   value=theta,
+		),           
+        
+        
+        # Slider for ts
+		html.Label('Initial condition = {}'.format(ts),
+ 				   id='ts_slider_text',
+ 				   style={'fontSize':size_slider_text}),  
+ 				   
+		dcc.Slider(id='ts_slider',
+ 				   min=ts_min, 
+ 				   max=ts_max, 
+ 				   # step=ts_step,
+ 				   # marks=ts_marks,
+ 				   value=ts
+		),],                  
+         
+		style={'width':'25%',
+			   'height':'800px',
 			   'fontSize':'10px',
-			   'padding-left':'5%',
-			   'padding-right':'5%',
+			   'padding-left':'3%',
+			   'padding-right':'2%',
 			   'padding-bottom':'0px',
                'padding-top':'40px',
 			   'vertical-align': 'middle',
-			   'display':'inline-block'},
+			   'display':'inline-block'
+               }
         ),
-
-
- 	# Phase map figure
- 	html.Div(
-		[dcc.Graph(id='phase_plot',
- 				   figure = fig_phase_map,
- 				   # config={'displayModeBar': False},
- 				   ),
- 		 ],
-		style={'width':'55%',
-			   'height':'500px',
-			   'fontSize':'10px',
-			   'padding-left':'0%',
-			   'padding-right':'5%',
-			   'vertical-align': 'middle',
-			   'display':'inline-block'},
- 	),
+        
+     
+    # Restitution plot
+   	html.Div(
+  		[dcc.Graph(id='fig_restitution',
+   				   figure = fig_restitution,
+   				   # config={'displayModeBar': False},
+   				   ),
+   		 ],
+  		style={'width':'30%',
+  			   'height':'800px',
+  			   'fontSize':'10px',
+  			   'padding-left':'0%',
+  			   'padding-right':'0%',
+  			   'vertical-align': 'middle',
+  			   'display':'inline-block'},
+   	),
+  
+   	# Cobweb plot
+   	html.Div(
+  		[dcc.Graph(id='fig_cobweb',
+   				   figure = fig_cobweb,
+   				   # config={'displayModeBar': False},
+   				   ),
+   		 ],
+  		style={'width':'30%',
+  			   'height':'800px',
+  			   'fontSize':'10px',
+  			   'padding-left':'0%',
+  			   'padding-right':'0%',
+  			   'vertical-align': 'middle',
+  			   'display':'inline-block'},
+   	),     
 
 
 ])
         
+        
+     
+     
+
+        
+        # # Model description
+        # html.P(model_text_2_line1,
+        #         style={'fontSize':15,
+        #               'color':'black',
+        #               # 'textAlign':'left',
+        #               }),
+        # html.Br(),
+        # html.P(model_text_2_line2,
+        #         style={'fontSize':15,
+        #               'color':'black',
+        #               # 'textAlign':'left',
+        #               }),        
+         
+        # html.Br(),
+        
+
+   
+        
+        
+        
+#         ],       
+# 		style={'width':'25%',
+# 			   'height':'800px',
+# 			   'fontSize':'10px',
+# 			   'padding-left':'0%',
+# 			   'padding-right':'0%',
+# 			   'padding-bottom':'0px',
+#                'padding-top':'40px',
+# 			   'vertical-align': 'middle',
+# 			   'display':'inline-block'},
+#         ),
+
+
+
 
 # #–-------------------
 # # Callback functions
@@ -219,59 +337,76 @@ app.layout = html.Div([
   
 # Update text for sliders             
 @app.callback(
-        [Output('phi0_slider_text','children'),
+        [
+         Output('apd0_slider_text','children'),
          Output('nmax_slider_text','children'),
+         Output('y0_slider_text','children'),
+         Output('a_slider_text','children'),
+         Output('b_slider_text','children'),
+         Output('theta_slider_text','children'),
+         Output('ts_slider_text','children'),
           ],
         [
-          Input('phi0_slider','value'),
+          Input('apd0_slider','value'),
           Input('nmax_slider','value'),
+          Input('y0_slider','value'),
+          Input('a_slider','value'),
+          Input('b_slider','value'),
+          Input('theta_slider','value'),
+          Input('ts_slider','value'),
           ]
 )
 
-def update_slider_text(phi0,nmax):
+def update_slider_text(apd0,nmax,y0,a,b,theta,ts):
     
     # Slider text update
-    text_phi0 = 'Initial condition = {}'.format(phi0)
-    text_nmax = 'Number of iterations = {}'.format(nmax)      
+    text_apd0 = 'APD_0 = {}'.format(apd0)
+    text_nmax = '# iterations = {}'.format(nmax)
+    text_y0 = 'y0 = {}'.format(y0)
+    text_a = 'a = {}'.format(a)
+    text_b = 'b = {}'.format(b)
+    text_theta = 'theta = {}'.format(theta)
+    text_ts = 'ts = {}'.format(ts)
 
-    return text_phi0, text_nmax
+    return text_apd0, text_nmax, text_y0, text_a, text_b, text_theta, text_ts
             
-         
+
 
 # Update figures
 @app.callback(
-            Output('phase_plot','figure'),
+            Output('fig_restitution','figure'),
+            Output('fig_cobweb','figure'),
             [
-              Input('phi0_slider','value'),
-              Input('nmax_slider','value'),           
+              Input('apd0_slider','value'),
+              Input('nmax_slider','value'),
+              Input('y0_slider','value'),
+              Input('a_slider','value'),
+              Input('b_slider','value'),
+              Input('theta_slider','value'),
+              Input('ts_slider','value'),        
             ],
             )
 
-def update_figs(phi0, nmax):
+def update_figs(apd0, nmax, y0, a, b, theta, ts):
     
-    # Phase map
-    def phase_map_specific(phi):
-        return phase_map(phi) 
-    
-    # Generate phi trajectory
-    phi_traj = generate_phase_trajectory(phase_map_specific, nmax, phi0)
-    
-        
-    # Make figure of phase map and stable trajectory
-    fig_phase_map = make_phase_map(phase_map_specific,
-                                   phi_traj,
-                                   )
 
-    return fig_phase_map
+    # Generate cobweb trajectory
+    apd_traj = generate_cobweb_trajectory(nmax, apd0, y0, a, b, theta, ts)
+    
+    # Make figures
+    fig_cobweb = make_cobweb_fig(y0, a, b, theta, ts, apd_traj)
+    fig_restitution = make_restitution_fig(y0, a, b)
 
+    return fig_restitution, fig_cobweb
 
 #-----------------
 # Add the server clause
 #–-----------------
-
 if __name__ == '__main__':
     app.run_server(
         debug=True,
         host='127.0.0.1',
         )
+
+
 
--- app_functions.py
+++ app_functions.py
@@ -13,73 +13,84 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-def phase_map(phi):
+
+
+
+
+
+
+def cobweb_map(apd, y0, a, b, theta, ts):
     '''
-    Function for the phase map phi{i+1} = f(phi_{i})
-    for modulated parasystole with delay.
+    Function for the cobweb map APD_{i+1} = f(APD_i)
 
     '''
     
-    if (0<=phi) and (phi<2/3):
-        phi_next = (3/2)*phi
-    else:
-        phi_next = (3/2)*phi - 1
+    # Use smallest N such that N*t_s-apd > theta
+    args = np.arange(1,10)*ts - apd
+    arg = args[args > theta][0]
+    
+    # Apply restitution curve
+    apd_next = y0 + a*(1-np.exp(-b*arg))
+    
+    return apd_next
 
-    return phi_next
 
 
 
-def generate_phase_trajectory(phase_map, nmax, phi0):
+
+def generate_cobweb_trajectory(nmax, apd0, y0, a, b, theta, ts):
     '''
-    Generate a phase trajectory by iterating the function phase_map.
+    Generate a cobweb trajectory by iterating the function cobweb_map.
     
     Input:
-        phase_map: function phi_{i+1} = f(phi_{i})
         nmax: number of iterations
-        phi0: initial condition
+        apd0: initial condition
     '''
     # Generate a phase trajectory
-    list_phi = []
-    phi=phi0
-    list_phi.append(phi0)
+    list_apd = []
+    apd=apd0
+    list_apd.append(apd0)
     for n in range(nmax):
-        phi = phase_map(phi)
-        list_phi.append(phi)
+        apd = cobweb_map(apd, y0, a, b, theta, ts)
+        list_apd.append(apd)
 
-    return list_phi
+    return list_apd
 
 
+# # Test
+# out = generate_cobweb_trajectory(cobweb_map, nmax, apd0, y0, a, b, theta, ts)
 
-def make_phase_map(phase_map,
-                   phi_traj,
-                   ):
+
+def make_cobweb_fig(y0, a, b, theta, ts,
+                    apd_traj,
+                    ):
     '''
-    Make phase map of model: modulated parasystole with conduction delay.
+    Make cobweb map of model (draw lines connecting subsequent states)
     Plot the trajectory that is converged to.
     
     Input:
-        phase_map: function phi_{i+1} = f(phi_{i})
-        phi_traj (list): trajectory of phi values
+        cobweb_map: function phi_{i+1} = f(phi_{i})
+        apd_traj (list): trajectory of apd values
         
     Ouptut:
         Plotly figure of map and stable trajectory
     '''
 
     # Create values for plot of phase map
-    xVals = np.linspace(0,1,1000)
-    yVals = np.array([phase_map(x) for x in xVals])
-    # Insert nan at discontinuities
-    pos = np.where(np.abs(np.diff(yVals)) >= 0.01)[0]
-    xVals[pos] = np.nan
-    yVals[pos] = np.nan
+    xVals = np.linspace(0,600,1000)
+    yVals = np.array([cobweb_map(x, y0, a, b, theta, ts) for x in xVals])
+    # # Insert nan at discontinuities
+    # pos = np.where(np.abs(np.diff(yVals)) >= 0.01)[0]
+    # xVals[pos] = np.nan
+    # yVals[pos] = np.nan
     
-    # Collect phi data and put in form for plotting lines
-    phi_traj_plot = []
+    # Collect apd data and put in form for plotting lines
+    apd_traj_plot = []
     # First line
-    phi_traj_plot.append((phi_traj[0],0))
-    for i in range(len(phi_traj)-1):
-        phi_traj_plot.append((phi_traj[i],phi_traj[i+1]))
-        phi_traj_plot.append((phi_traj[i+1],phi_traj[i+1]))
+    apd_traj_plot.append((apd_traj[0],0))
+    for i in range(len(apd_traj)-1):
+        apd_traj_plot.append((apd_traj[i],apd_traj[i+1]))
+        apd_traj_plot.append((apd_traj[i+1],apd_traj[i+1]))
 
 
     fig = go.Figure()
@@ -95,8 +106,8 @@ def make_phase_map(phase_map,
     
     # Trace for line y=x
     fig.add_trace(
-        go.Scatter(x=np.linspace(0,1,100),
-                   y=np.linspace(0,1,100),
+        go.Scatter(x=np.linspace(0,600,1000),
+                   y=np.linspace(0,600,1000),
                    line={'dash':'dash',
                          'color':'gray'},
                    showlegend=False,
@@ -105,8 +116,8 @@ def make_phase_map(phase_map,
     
     # Trace for phase trajectory
     fig.add_trace(
-        go.Scatter(x=[tup[0] for tup in phi_traj_plot],
-                   y=[tup[1] for tup in phi_traj_plot],
+        go.Scatter(x=[tup[0] for tup in apd_traj_plot],
+                   y=[tup[1] for tup in apd_traj_plot],
                    showlegend=False,
                    line={'color':'royalblue'},
         )
@@ -114,23 +125,55 @@ def make_phase_map(phase_map,
     
     
     fig.update_xaxes(
-        range=[0,1],
-        title='x_{t}',
+        range=[0,250],
+        title='APD_{i}',
     )
     
     fig.update_yaxes(
-        range=[0,1],
-        title='x_{t+1}',
+        range=[0,250],
+        title='APD_{i+1}',
     )
     
     fig.update_layout(
-        width=500,
-        height=500,
-        # title='Cobweb plot',
+        # width=500, height=500,
+        margin=dict(l=50,r=10,t=70,b=10),
         )
+
 
        
     return fig
+
+
+
+
+
+def make_restitution_fig(y0=120, a=96.9, b=0.0104):
+    
+    # Create values for plot of phase map
+    x = np.linspace(0,300,1000)
+    y = y0 + a*(1-np.exp(-b*x))
+    
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(x=x, y=y,
+                   showlegend=False,
+                   mode='lines',
+                   )
+    )
+    fig.update_xaxes(title = 'DI (ms)', range=[0,300])
+    fig.update_yaxes(title = 'APD (ms)', range=[50,250])
+    
+    fig.update_layout(
+        # width=500, height=500,
+        margin=dict(l=50,r=10,t=70,b=10),
+        )
+    
+    return fig    
+
+
+
+
+
 
 
 
@@ -141,16 +184,28 @@ def make_phase_map(phase_map,
 
 
 # # Test function to generate a trajectory
-# phi0=0.2
-# nmax=10
-# phi_traj = generate_phase_trajectory(phase_map, nmax, phi0)
+# y0=120
+# a=96.9
+# b=0.0104
+# theta = 20
+# ts = 400
+
+# apd0 = 300
+# nmax = 100
+
+
+
+# apd_traj = generate_cobweb_trajectory(nmax, apd0, y0, a, b, theta, ts)
 
 
 # # Test function to make figure of phase map
-# fig_phase_map = make_phase_map(phase_map,
-#                                 phi_traj,
-#                                 )
-# fig_phase_map.write_html('temp.html')
+# fig_cobweb_map = make_cobweb_fig(y0, a, b, theta, ts, apd_traj)
+# fig_cobweb_map.write_html('temp.html')
 
 
+
+
+
+# fig = make_restitution_plot()
+# fig.write_html('temp.html')
 
